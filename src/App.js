9bb0fccdc867b8a3d5b7f85a7f570f0efc358562
carrot3000 <jjy306105@gmail.com>
2021-02-16 16:25:41 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, {Component} from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody'; //tablebody에 각각의 모델이 저장되어 보여짐
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+  root: {
+    width:'100%',
+    marginTop: theme.spacing.unit *3,
+    overflowX: "auto"
+  },
+    table:{
+      minWidth:1080 //최소 1080 픽샐  화면 사이즈 작아지면 so 가로스크롤바 생김
+
+  }
+
+})
+
 
 const customers =[ { //배열형태로
   'id': 1,
@@ -27,42 +49,45 @@ const customers =[ { //배열형태로
   'gender':'여자',
   'job': '개발자'
 }
-
-
-
 ]
 
 class App extends Component {
   render(){
+    const {classes} = this.props;
   return (
-    <div>
-      {
-        customers.map(c=>{ //customers를 C로 순회! 반복처리
-          return(
-          <Customer
-            key = {c.id} //모두 다른값을 가지는 id를 KEY 값으로 설정해 사용
-            id={c.id}
-            image={c.image}
-            name = {c.name}
-            birthday = {c.birthday}
-            gender = {c.gender}
-            job = {c.job}
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
 
-
-            />
-
-
-
-        );
-
-      })
+        </TableHead>
+        <TableBody>
+        {
+          customers.map(c=>{ //customers를 C로 순회! 반복처리
+            return(
+            <Customer
+              key = {c.id} //모두 다른값을 가지는 id를 KEY 값으로 설정해 사용
+              id={c.id}
+              image={c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+              />
+          );
+        })    
+        }
+      </TableBody>
+      </Table>
       
-      }
-
-
-    </div>  
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,40 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     render(){//render는 return이 있는 형식임
         return(
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo bitrhday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+            <Fragment>
+                <TableRow>
+                    <TableCell>{this.props.id}</TableCell>
+                    <TableCell><img src ={this.props.image} alt="profile"/> </TableCell>
+                    <TableCell>{this.props.name} </TableCell>
+                    <TableCell>{this.props.birthday} </TableCell>
+                    <TableCell>{this.props.gender} </TableCell>
+                    <TableCell>{this.props.job} </TableCell>
+                </TableRow>
+            </Fragment>
         )
     }
-
 }
 
-class CustomerProfile extends React.Component{
-    render(){//render는 return이 있는 형식임
-        return(
-            <div>
-                <img src={this.props.image} alt = "profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-
-}
-
-
-class CustomerInfo extends React.Component{
-    render(){//render는 return이 있는 형식임
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-
-}
 export default Customer;
